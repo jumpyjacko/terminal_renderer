c6b562b40e1dbf55dc72d9bf92c28d65f4fdfe13
Jackson Ly <48436180+JumpyJacko@users.noreply.github.com>
2023-04-18 15:29:40 +1000

    Normalised vectors

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::math::{Float3, Int2};
+use crate::math::{calculate_face_normal, Float3, Int2};
 use crate::shapes::Face;
 use std::{thread, time::Duration, time::Instant};
 
@@ -9,12 +9,6 @@ const TILES: [&str; 6] = [" ", ".", ",", "-", "=", "#"];
 const SIZE: usize = 30;
 const FOCAL_LENGTH: i32 = 30;
 
-const LIGHT_VECTOR: Float3 = Float3 {
-    x: -1.0,
-    y: -1.0,
-    z: -1.0,
-};
-
 const OFFSET: usize = SIZE / 2;
 
 fn main() {
@@ -23,6 +17,13 @@ fn main() {
     let vert_table: Vec<Float3> = shapes::PLANE.verts.to_vec();
     let face_table: Vec<Face> = shapes::PLANE.faces.to_vec();
 
+    let light_vector: Float3 = Float3 {
+        x: -1.0,
+        y: -1.0,
+        z: -1.0,
+    }
+    .normalise();
+
     let mut theta: f32 = 0.0;
 
     loop {
@@ -30,19 +31,31 @@ fn main() {
 
         let mut canvas = [[0; SIZE]; SIZE];
 
-        let mut rotated_vert_table: Vec<Float3> = vec![];
+        let mut transformed_vert_table: Vec<Float3> = vec![];
         let mut projected_vert_table: Vec<Int2> = vec![];
 
         vert_table.iter().for_each(|v| {
-            let rotated_point = v.rotate_y(&theta);
-            rotated_vert_table.push(rotated_point);
+            let transformed_point = v.rotate_y(&theta);
+            transformed_vert_table.push(transformed_point);
         });
 
-        rotated_vert_table.iter().for_each(|v| {
+        transformed_vert_table.iter().for_each(|v| {
             let p_point = v.project(&FOCAL_LENGTH);
             // println!("{}, {}", p_point.x, p_point.y);
             projected_vert_table.push(p_point);
         });
+
+        // Normal Calculations
+        for face in &face_table {
+            let normal = calculate_face_normal(
+                transformed_vert_table[face.a],
+                transformed_vert_table[face.b],
+                transformed_vert_table[face.c],
+            );
+
+            let dot_product =
+                normal.x * light_vector.x + normal.y * light_vector.y + normal.z * light_vector.z;
+        }
 
         // Face Drawing
         for y in 0..SIZE {
@@ -62,18 +75,6 @@ fn main() {
                         projected_vert_table[face.c],
                     );
 
-                    // println!(
-                    //     "p: {}, {} | tri: ({}, {}), ({}, {}), ({}, {}) | result: {}",
-                    //     p.x,
-                    //     p.y,
-                    //     projected_vert_table[face.a].x,
-                    //     projected_vert_table[face.a].y,
-                    //     projected_vert_table[face.b].x,
-                    //     projected_vert_table[face.b].y,
-                    //     projected_vert_table[face.c].x,
-                    //     projected_vert_table[face.c].y,
-                    //     result
-                    // );
                     if result {
                         canvas[y][x] = 1;
                     }
@@ -88,7 +89,7 @@ fn main() {
             println!();
         }
 
-        theta += 0.05;
+        // theta += 0.05;
 
         thread::sleep(Duration::from_millis(30))
     }
--- src/math.rs
+++ src/math.rs
@@ -23,6 +23,15 @@ impl Float3 {
         projected_point
     }
 
+    pub fn normalise(&self) -> Float3 {
+        let length = (self.x * self.x + self.y * self.y + self.z * self.z).sqrt();
+        Float3 {
+            x: self.x / length,
+            y: self.y / length,
+            z: self.z / length,
+        }
+    }
+
     // All three of these rotation functions use the simplified per-axis rotation matrix
     pub fn rotate_x(&self, theta: &f32) -> Float3 {
         let yz = &[self.y, self.z];
@@ -75,27 +84,6 @@ impl Float3 {
 
 impl Int2 {
     pub fn in_tri(&self, p0: Int2, p1: Int2, p2: Int2) -> bool {
-        // let d_x = self.x - p2.x;
-        // let d_y = self.y - p2.y;
-        // let d_x21 = p2.x - p1.x;
-        // let d_y12 = p1.y - p2.y;
-        // let d = d_y12 * (p0.x - p2.x) + d_x21 * (p0.y - p2.y);
-        // let s = d_y12 * d_x + d_x21 * d_y;
-        // let t = d_x * (p2.y - p0.y) + d_y * (p0.x - p2.x);
-        // if d < 0 {
-        //     return s <= 0 && t <= 0 && s + t <= d;
-        // }
-        // return s >= 0 && t >= 0 && s + t <= d;
-
-        // let a = 1 / 2 * (-p1.y * p2.x + p0.y * (-p1.x + p2.x) + p0.x * (p1.y - p2.y) + p1.x * p2.y);
-        // let sign = if a < 0 { -1 } else { 1 };
-        // let s =
-        //     (p0.y * p2.x - p0.x * p2.y + (p2.y - p0.y) * self.x + (p0.x - p2.x) * self.y) * sign;
-        // let t =
-        //     (p0.x * p1.y - p0.y * p1.x + (p0.y - p1.y) * self.x + (p1.x - p0.x) * self.y) * sign;
-        //
-        // return s > 0 && t > 0 && (s + t) < 2 * a * sign;
-
         let side_1 = (self.x - p1.x) * (p0.y - p1.y) - (p0.x - p1.x) * (self.y - p1.y);
         let side_2 = (self.x - p2.x) * (p1.y - p2.y) - (p1.x - p2.x) * (self.y - p2.y);
         let side_3 = (self.x - p0.x) * (p2.y - p0.y) - (p2.x - p0.x) * (self.y - p0.y);
@@ -111,18 +99,23 @@ pub fn calculate_face_normal(p0: Float3, p1: Float3, p2: Float3) -> Float3 {
         y: p1.y - p0.y,
         z: p1.z - p0.z,
     };
+
     let v = Float3 {
         x: p2.x - p0.x,
         y: p2.y - p0.y,
         z: p2.z - p0.z,
     };
-    let nx = u.y * v.z - u.z * v.y;
-    let ny = u.z * v.x - u.x * v.z;
-    let nz = u.x * v.y - u.y * v.x;
 
-    Float3 {
-        x: nx,
-        y: ny,
-        z: nz,
-    }
+    let n = Float3 {
+        x: u.y * v.z - u.z * v.y,
+        y: u.z * v.x - u.x * v.z,
+        z: u.x * v.y - u.y * v.x,
+    };
+
+    println!(
+        "U: {}, {}, {} | V: {}, {}, {} | N: {}, {}, {}",
+        u.x, u.y, u.z, v.x, v.y, v.z, n.x, n.y, n.z
+    );
+
+    n.normalise()
 }
